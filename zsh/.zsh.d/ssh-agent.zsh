a21792978c0bb85c68b0de70c40b70a39698c41f
Tim Humphries <tim@utf8.me>
2018-04-07 11:43:50 -0700

    ssh-reagent

--- zsh/.zsh.d/ssh-agent.zsh
+++ zsh/.zsh.d/ssh-agent.zsh
@@ -1,1 +1,9 @@
-eval `ssh-agent`
+ssh-reagent () {
+  for agent in /tmp/ssh-*/agent.*; do
+    export SSH_AUTH_SOCK=$agent
+    if ssh-add -l 2>&1 > /dev/null; then return; fi
+  done
+  eval `ssh-agent`
+}
+
+ssh-reagent
