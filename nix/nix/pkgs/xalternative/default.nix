edc5f8e25452f6f482aade72b5865ee62382ae7f
Tim Humphries <tim@utf8.me>
2018-04-07 13:35:17 -0700

    callPackage

--- nix/nix/pkgs/xalternative/default.nix
+++ nix/nix/pkgs/xalternative/default.nix
@@ -1,7 +1,10 @@
-{ pkgs }:
-pkgs.fetchgit {
-  url = "https://github.com/thumphries/xalternative";
-  rev = "3b70fe7cc7c2ee0f0b8c7a3072b5c512e90bd54d";
-  sha256 = "1zcc71alicrxnhjd2qn48f7b7w1gbbdmq4kw30fsxnl71gcp5c2q";
-  fetchSubmodules = true;
-}
+{ fetchgit, callPackage }:
+let
+  src = fetchgit {
+    url = "https://github.com/thumphries/xalternative";
+    rev = "3b70fe7cc7c2ee0f0b8c7a3072b5c512e90bd54d";
+    sha256 = "1zcc71alicrxnhjd2qn48f7b7w1gbbdmq4kw30fsxnl71gcp5c2q";
+    fetchSubmodules = true;
+  };
+in
+  callPackage src { }
