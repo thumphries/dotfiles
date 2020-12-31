ca363e5deae6746d1a795d3190416848eb0a1057
Tim Humphries <tim@utf8.me>
2020-12-30 17:30:22 -0800

    Remove busted ssh-reagent script

--- zsh/.zsh.d/ssh-agent.zsh
+++
@@ -1,9 +0,0 @@
-ssh-reagent () {
-  for agent in /tmp/ssh-*/agent.*; do
-    export SSH_AUTH_SOCK=$agent
-    if ssh-add -l 2>&1 > /dev/null; then return; fi
-  done
-  eval `ssh-agent`
-}
-
-ssh-reagent
